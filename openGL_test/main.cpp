6bfd26ad84c9699b8906048c4d61a0d94aae03c5
zhuangjun1981 <wood_stocker@hotmail.com>
2020-06-19 14:44:44 -0700

    testing minimum

--- openGL_test/main.cpp
+++ openGL_test/main.cpp
@@ -2,39 +2,12 @@
 
 Vertex vertices[] =
 {
-	//Position							//color							//Texcoords
-	
-	//Triangle1
-	glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),		glm::vec2(0.f, 1.f),
-	glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),		glm::vec2(0.f, 0.f),
-	glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),		glm::vec2(1.f, 0.f),
-
-	//Triangle2
-	//glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),		glm::vec2(0.f, 1.f),
-	//glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),		glm::vec2(1.f, 0.f),
-	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),		glm::vec2(1.f, 1.f)
+	//Position					
+	glm::vec3(-0.5f, 0.5f, 0.f),
+	glm::vec3(-0.5f, -0.5f, 0.f),
+	glm::vec3(0.5f, -0.5f, 0.f),
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
-
-GLuint indices[] =
-{
-	0, 1, 2,  //Triangle1
-	0, 2, 3   //Triangle2
-};
-unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
-
-void updateInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	}
-}
-
-void framebuffer_resize_callback(GLFWwindow* window, int fbW, int fbH)
-{
-	glViewport(0, 0, fbW, fbH);
-}
 
 bool loadShaders(GLuint &program)
 {
@@ -43,13 +16,13 @@ bool loadShaders(GLuint &program)
 	char infoLog[512];
 	GLint success;
 
-    std::string temp = "";
+	std::string temp = "";
 	std::string src = "";
 
 	std::ifstream in_file;
 
 	//Vertex
-	in_file.open("vertex_core.glsl");
+	in_file.open("vertex_minimum.glsl");
 	if (in_file.is_open())
 	{
 		while (std::getline(in_file, temp))
@@ -80,7 +53,7 @@ bool loadShaders(GLuint &program)
 	temp = "";
 	src = "";
 
-	in_file.open("fragment_core.glsl");
+	in_file.open("fragment_minimum.glsl");
 	if (in_file.is_open())
 	{
 		while (std::getline(in_file, temp))
@@ -125,8 +98,6 @@ bool loadShaders(GLuint &program)
 	}
 
 	//End
-	std::cout << "size of vertexShader: " << sizeof(vertexShader) << std::endl;
-	std::cout << "size of fragmentShader: " << sizeof(fragmentShader) << std::endl;
 	glUseProgram(0);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
@@ -162,12 +133,9 @@ int main()
 		return -1;
 	}
 
-	//The following is for adjustable window size
-	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
-
 	//The following is for fixed window size
-	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
-	//glViewport(0, 0, framebufferWidth, framebufferHeight);
+	glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+	glViewport(0, 0, framebufferWidth, framebufferHeight);
 
 	glfwMakeContextCurrent(window); //important
 
@@ -203,7 +171,7 @@ int main()
 
 	//VAO, VBO, EBO
 	//GEN VAO AND BIND
-	GLuint VAO; 
+	GLuint VAO;
 	glCreateVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
@@ -215,18 +183,8 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	std::cout << "VBO: " << VBO << std::endl;
-
-	//GEN EBO AND BIND AND SEND DATA
-	GLuint EBO;
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	std::cout << "EBO: " << EBO << std::endl;
-
 	//SET VERTEXATTRIBPOINTERS AND ENABLE (INPUT ASSEMBLY)
-	
+
 	//GLuint attribLoc = glGetAttribLocation(core_program, "vertex_position");
 	//glVertexAttribPointer(attribLoc, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
 	//glEnableVertexAttribArray(attribLoc);
@@ -235,59 +193,13 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
 	glEnableVertexAttribArray(0);
 
-	//Color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
-	glEnableVertexAttribArray(1);
-
-	//Texcoord
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
-	glEnableVertexAttribArray(2);
-
 	//BIND VAO 0, release VOA
 	glBindVertexArray(0);
 
-	//TEXTURE INIT
-	int image_width = 0;
-	int image_height = 0;
-	unsigned char* image = SOIL_load_image("Images/kevin_and_yu.png", 
-		&image_width, &image_height, NULL, SOIL_LOAD_RGBA);
 
-	GLuint texture0;
-	glGenTextures(1, &texture0);
-	glBindTexture(GL_TEXTURE_2D, texture0);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-	if (image)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, 
-			image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << std::endl;
-	}
-
-	glActiveTexture(0);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	SOIL_free_image_data(image);
-
-	
 	//MAIN LOOP
-	while (!glfwWindowShouldClose(window)) 
+	while (!glfwWindowShouldClose(window))
 	{
-		//std::cout << "one frame" << std::endl;
-		
-		//UPDATE INPUT ---
-		glfwPollEvents();
-
-		//UPDATE ---
-		updateInput(window);
-
 		//DRAW ---
 
 		//Clear
@@ -298,18 +210,17 @@ int main()
 		glUseProgram(core_program);
 
 		//Update uniforms
-		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
+		//glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
 
 		//Activate texture
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture0);
+		//glActiveTexture(GL_TEXTURE0);
+		//glBindTexture(GL_TEXTURE_2D, texture0);
 
 		//Bind vertex array object
 		glBindVertexArray(VAO);
-		
+
 		//Draw
-		//glDrawArrays(GL_TRIANGLES, 0, nrOfVertices);
-		glDrawElements(GL_TRIANGLES, nrOfIndices, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, nrOfVertices);
 
 		//End Draw
 		glfwSwapBuffers(window);
--- openGL_test/vertex_minimum.glsl
+++ openGL_test/vertex_minimum.glsl
@@ -4,5 +4,5 @@ layout(location = 0) in vec3 aPos;
 
 void main()
 {
-	lg_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
 }
